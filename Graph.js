5bd783f05b8c6c73aa3b6cb4fef810483a478ff2
Nishimura-Kazuhiko <pico_pong@icloud.com>
2024-10-06 22:20:21 +0900

    [PASS] Add Graph class and draw check

---
+++ Graph.js
@@ -0,0 +1,95 @@
+class Graph
+{
+
+    constructor()
+    {
+        this.reset();
+    }
+    
+    reset()
+    {
+        this.nodeNameArray = [];
+        this.edgeNameArray = [];
+        this.nodeArray = [];
+        this.edgeArray = [];
+        this.nodeIndex = 0;
+        this.edgeIndex = 0;
+    }
+    
+    addNodeByParam(name, x, y)
+    {
+        let node0 = new Node(name, x, y);
+        return this.addNode(node0);
+    }
+    
+    // add success true   fail false
+    addNode(node0)
+    {
+        if (!this.nodeNameArray.includes(node0.name)) {
+            this.nodeNameArray.push(node0.name);
+            this.nodeArray.push(node0);
+            this.nodeIndex++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    addNodeByIndices(name, index0, index1, type)
+    {
+        // same index
+        if(index0==index1){
+            return false;
+        }
+        
+        // index out of range
+        if(index0<0){
+            return false;
+        }
+        if(index0>=this.nodeIndex){
+            return false;
+        }
+        if(index1<0){
+            return false;
+        }
+        if(index1>=this.nodeIndex){
+            return false;
+        }
+        
+        let edge0 = new Edge(name, this.nodeArray[index0], this.nodeArray[index1], type);
+        return this.addEdge(edge0);
+    }
+    
+    // add success true   fail false
+    addEdge(edge0)
+    {
+        if (!this.edgeNameArray.includes(edge0.name)) {
+            this.edgeNameArray.push(edge0.name);
+            this.edgeArray.push(edge0);
+            this.edgeIndex++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    drawGraph()
+    {
+        this.drawEdges();
+        this.drawNodes();
+    }
+    
+    drawEdges()
+    {
+        for (const edge0 of this.edgeArray) {
+            edge0.drawEdge();
+        }
+    }
+
+    drawNodes()
+    {
+        for (const node0 of this.nodeArray) {
+            node0.drawNode();
+        }
+    }
+}
--- switch_node.js
+++ switch_node.js
@@ -1,10 +1,97 @@
+// main
+let config;
+let drawFunc;
+
+
+// for debug
+let _debug_state;
+// _debug_state = 'draw_test';
+// _debug_test_graph00()
+// _debug_test_graph_move00()
+let _graph;
+
 function setup() 
 {
+    config   = new Config();
+    drawFunc = new DrawFunc();
 
+    createCanvas(1024, 768);
+    background(255);
+    
+    _debug_state = 'draw_test';
+    _debug_test_graph00();
 }
 
 
 function draw()
 {
+    _debug_test_graph_move00();
+}
 
+
+// for debug functions
+// make node and edge, and draw them
+function _debug_test_graph00()
+{
+    if(_debug_state == 'draw_test'){
+        _graph = new Graph();
+        
+        let nodeIndex = 0;
+        let edgeIndex = 0;
+        let xiMax = 4;
+        let yiMax = 4;
+        
+        for (let yi=0; yi<yiMax; yi++) {
+            for (let xi=0; xi<xiMax; xi++) {
+                let nodeName = 'RL' + nodeIndex.toString();
+                let x = width*(xi+1)/(xiMax+2);
+                let y = height*(yi+1)/(yiMax+2);
+                _graph.addNodeByParam(nodeName,x,y);
+                nodeIndex++;
+            }
+        }
+        
+        for(let i=0;i<_graph.nodeIndex;i++){
+            let name;
+            let index0;
+            
+            if(i/yiMax<yiMax-1){
+                name = 'Edge' + edgeIndex.toString();
+                index0 = i+xiMax;
+                _graph.addNodeByIndices(name,i,index0,'undirected');
+                edgeIndex++;
+            }
+            
+            if(i%xiMax<xiMax-1){
+                name = 'Edge' + edgeIndex.toString();
+                index0 = i+1;
+                _graph.addNodeByIndices(name,i,index0,'undirected');
+                edgeIndex++;
+            }
+            
+            if(i/yiMax<yiMax-1 && i%xiMax<xiMax-1){
+                name = 'Edge' + edgeIndex.toString();
+                index0 = i+xiMax+1;
+                _graph.addNodeByIndices(name,i,index0,'undirected');
+                edgeIndex++;
+            }
+        }
+        
+        _graph.drawGraph();
+    }
+}
+
+
+// jitter function
+function _debug_test_graph_move00()
+{
+    if(_debug_state == 'draw_test'){
+        background(255);
+        
+        for(let node0 of _graph.nodeArray){
+            node0.setX(node0.x+random(-2,2));
+            node0.setY(node0.y+random(-2,2));
+        }
+        _graph.drawGraph();
+    }
 }
